2be76c57da9239124bd16ba390ef6fda6909dac3
jomoetnt <jomoetnt@gmail.com>
2024-05-25 02:52:12 -0400

    input system and movement

--- scripts/fighters/fighter.gd
+++ scripts/fighters/fighter.gd
@@ -1,7 +1,9 @@
 class_name fighter extends CharacterBody3D
 
-const SPEED = 2.0
+const WALK_SPEED = 2.0
 const JUMP_VELOCITY = 4.5
+const PREJUMP_FRAMES = 5
+const LAND_FRAMES = 4
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -12,33 +14,68 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var health = 500
 var mana = 0
 
+var actionable = true
+var actionableTimer = 0
+
 var player = 1
 
-enum direction {UP, DOWN, LEFT, RIGHT, UPLEFT, UPRIGHT, DOWNLEFT, DOWNRIGHT, NEUTRAL}
-enum button {light_punch, light_kick, heavy_punch, heavy_kick, enhance, throw, throw_swap, ultimate, start, ready, none}
-enum universalMoves {light_punch, light_kick, heavy_punch, heavy_kick, throw, throw_swap, ultimate}
-var curDir = direction.NEUTRAL
-var curBtn = button.none
+var moves = {
+	"Nothing": commandMove.new(0, 0, 0),
+	"Jump": commandMove.new(PREJUMP_FRAMES, 0, 0),
+	"Jump Land": commandMove.new(0, 0, LAND_FRAMES),
+	"Light Punch": commandMove.new(0, 0, 0),
+	"Heavy Punch": commandMove.new(0, 0, 0) 
+}
 
-var inputBuffer = []
+enum direction {UP, DOWN, LEFT, RIGHT, UPLEFT, UPRIGHT, DOWNLEFT, DOWNRIGHT, NEUTRAL}
+enum button {LIGHT_PUNCH, LIGHT_KICK, HEAVY_PUNCH, HEAVY_KICK, ENHANCE, THROW, THROW_SWAP, ULTIMATE_IGNITE, START, READY, NONE}
+var curDir = direction.NEUTRAL
+var curBtn = button.NONE
+var curMove = moves["Nothing"]
+
+var inputBuffer:Array[motionInput]
+
+signal inputSignal(inputString)
 
 class motionInput:
 	var inputDirection:direction
 	var inputButton:button
+	var lifetime:int
 	func _init(inputDir:direction, inputBtn:button):
 		inputDirection = inputDir
 		inputButton = inputBtn
+		lifetime = determine_lifetime()
+	func determine_lifetime():
+		return 30
+	func _to_string():
+		if inputButton != button.NONE:
+			return direction.keys()[inputDirection] + " + " + button.keys()[inputButton]
+		if inputDirection != direction.NEUTRAL:
+			return direction.keys()[inputDirection]
+		return ""
+		
+
+class commandMove:
+	var commandInputs = []
+	var startupFrames = 0
+	var activeFrames = 0
+	var recoveryFrames:int
+	func _init(startup:int, active:int, recovery:int):
+		startupFrames = startup
+		activeFrames = active
+		recoveryFrames = recovery
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+
+	updateState()
 		
 	# Get the input direction
 	var input_dir:Vector2
 	if player == 1:
 		input_dir = Input.get_vector("p1_left", "p1_right", "p1_down", "p1_up")
-		print(input_dir)
 	else:
 		input_dir = Input.get_vector("p2_left", "p2_right", "p2_down", "p2_up")
 	
@@ -67,99 +104,135 @@ func _physics_process(delta):
 	curBtn = parse_btn()
 	
 	var curInput = motionInput.new(curDir, curBtn)
-	process_input(curInput, SPEED * input_dir.x)
-
-	#Handles land
-	if anim_state.get_current_node() == "jump_idle" and is_on_floor():
-		land()
+	process_input(curInput, WALK_SPEED * input_dir.x)
 	
 	move_and_slide()
 
-func jump():
+func isAnimationFinished():
+	return anim_state.get_current_play_position() >= anim_state.get_current_length()
+	
+func updateState():
+	if actionableTimer > 0:
+		actionableTimer -= 1
+		
+	if actionableTimer == 0:
+		actionable = true
+		curMove = moves["Nothing"]
+	
+	if anim_state.get_current_node() == "jump" and isAnimationFinished():
+		anim_state.travel("jump_idle")
+	
+	if anim_state.get_current_node() == "jump_land" and isAnimationFinished():
+		anim_state.travel("BlendSpace1D")
+	
+	if anim_state.get_current_node() == "crouch" and isAnimationFinished():
+		anim_state.travel("crouch_idle")
+		
+	if anim_state.get_current_node() == "jump_idle" and is_on_floor():
+		land()
+
+func jump(xDirection):
 	velocity.y = JUMP_VELOCITY
+	velocity.x = xDirection * WALK_SPEED * 2
+	curMove = moves["Jump"]
+	actionable = false
+	actionableTimer = curMove.startupFrames
 	anim_state.travel("jump")
 	
 func land():
 	anim_state.travel("jump_land")
 	velocity.x = 0
+	curMove = moves["Jump Land"]
+	actionableTimer = curMove.recoveryFrames
+	actionable = false
 	
 func crouch():
 	velocity.x = 0
 	anim_state.travel("crouch")
+	curMove = moves["Nothing"]
 	
 func walk(speed):
 	anim_state.travel("BlendSpace1D")
 	velocity.x = speed
 	if player == 1:
-		animation_tree.set("parameters/BlendSpace1D/blend_position", speed / SPEED)
+		animation_tree.set("parameters/BlendSpace1D/blend_position", speed / WALK_SPEED)
 	else:
-		animation_tree.set("parameters/BlendSpace1D/blend_position", -speed / SPEED)
+		animation_tree.set("parameters/BlendSpace1D/blend_position", -speed / WALK_SPEED)
+	curMove = moves["Nothing"]
+		
+func handle_movement(motInput:motionInput, speed):
+	match motInput.inputDirection:
+		direction.NEUTRAL:
+			if curMove == moves["Nothing"] and anim_state.get_current_node() != "jump_land":
+				walk(0)
+		direction.RIGHT, direction.LEFT:
+			if curMove == moves["Nothing"]:
+				walk(speed)
+		direction.UPLEFT:
+			if curMove == moves["Nothing"]:
+				jump(-1)
+		direction.UP:
+			if curMove == moves["Nothing"]:
+				jump(0)
+		direction.UPRIGHT:
+			if curMove == moves["Nothing"]:
+				jump(1)
+		direction.DOWNLEFT, direction.DOWN, direction.DOWNRIGHT:
+			if curMove == moves["Nothing"] and anim_state.get_current_node() != "crouch_idle" and anim_state.get_current_node() != "crouch":
+				crouch()
 
 func execute_move(move:StringName):
+	velocity.x = 0
 	match move:
 		"heavy_punch":
 			anim_state.travel("heavy_punch")
+			curMove = moves["Heavy Punch"]
+			actionable = false
+			actionableTimer = curMove.recoveryFrames
 		"light_punch":
 			anim_state.travel("light_punch")
+			curMove = moves["Light Punch"]
+			actionable = false
+			actionableTimer = curMove.recoveryFrames
 
 func parse_btn():
 	if player == 1:
 		if Input.is_action_just_pressed("p1_heavy_kick"):
-			return button.heavy_kick
+			return button.HEAVY_KICK
 		if Input.is_action_just_pressed("p1_light_kick"):
-			return button.light_kick
+			return button.LIGHT_KICK
 		if Input.is_action_just_pressed("p1_heavy_punch"):
-			return button.heavy_punch
+			return button.HEAVY_PUNCH
 		if Input.is_action_just_pressed("p1_light_punch"):
-			return button.light_punch
+			return button.LIGHT_PUNCH
 	else:
 		if Input.is_action_just_pressed("p2_heavy_kick"):
-			return button.heavy_kick
+			return button.HEAVY_KICK
 		if Input.is_action_just_pressed("p2_light_kick"):
-			return button.light_kick
+			return button.LIGHT_KICK
 		if Input.is_action_just_pressed("p2_heavy_punch"):
-			return button.heavy_punch
+			return button.HEAVY_PUNCH
 		if Input.is_action_just_pressed("p2_light_punch"):
-			return button.light_punch
-	return button.none
+			return button.LIGHT_PUNCH
+	return button.NONE
 
 func process_input(motInput:motionInput, speed):
-	match dir:
-		direction.UP:
-			if anim_state.get_current_node() == "BlendSpace1D" || anim_state.get_current_node() == "crouch_idle":
-				jump()
-		direction.UPRIGHT:
-			if anim_state.get_current_node() == "BlendSpace1D" || anim_state.get_current_node() == "crouch_idle":
-				velocity.x = speed * 2
-				jump()
-		direction.UPLEFT:
-			if anim_state.get_current_node() == "BlendSpace1D" || anim_state.get_current_node() == "crouch_idle":
-				velocity.x = speed * 2
-				jump()
-		direction.DOWN:
-			if anim_state.get_current_node() == "BlendSpace1D":
-				crouch()
-		direction.DOWNLEFT:
-			if anim_state.get_current_node() == "BlendSpace1D":
-				crouch()
-		direction.DOWNRIGHT:
-			if anim_state.get_current_node() == "BlendSpace1D":
-				crouch()
-		direction.LEFT:
-			if anim_state.get_current_node() == "BlendSpace1D" || anim_state.get_current_node() == "crouch_idle":
-				walk(speed)
-		direction.RIGHT:
-			if anim_state.get_current_node() == "BlendSpace1D" || anim_state.get_current_node() == "crouch_idle":
-				walk(speed)
-		direction.NEUTRAL:
-			match btn:
-				button.heavy_punch:
-					if anim_state.get_current_node() == "BlendSpace1D":
-						execute_move("heavy_punch")
-				button.light_punch:
-					if anim_state.get_current_node() == "BlendSpace1D":
-						execute_move("light_punch")
-				_:
-					if anim_state.get_current_node() == "BlendSpace1D" || anim_state.get_current_node() == "crouch_idle":
-						walk(0)
-			
+	inputSignal.emit(str(motInput))
+	if !actionable:
+		inputBuffer.append(motInput)
+		return
+	for input in inputBuffer:
+		input.lifetime -= 1
+		if input.lifetime <= 0:
+			inputBuffer.erase(input)
+	
+	match motInput.inputButton:
+		button.NONE:
+			if anim_state.get_current_node() != "jump_idle" and anim_state.get_current_node() != "jump":
+				handle_movement(motInput, speed)
+		button.HEAVY_PUNCH:
+			if anim_state.get_current_node() != "jump_idle":
+				execute_move("heavy_punch")
+		button.LIGHT_PUNCH:
+			if anim_state.get_current_node() != "jump_idle":
+				execute_move("light_punch")
--- scripts/fighters/killer_bean.gd
+++ scripts/fighters/killer_bean.gd
@@ -1,3 +1,6 @@
 class_name KillerBeanFighter extends "fighter.gd"
 
 
+func _init():
+	moves["Light Punch"].recoveryFrames = 17
+	moves["Heavy Punch"].recoveryFrames = 36
