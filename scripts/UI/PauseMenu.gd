ed764cc7b8052b504291e30b1233e2c0abc48aba
jomoetnt <jomoetnt@gmail.com>
2024-06-01 01:48:25 -0400

    menus

---
+++ scripts/UI/PauseMenu.gd
@@ -0,0 +1,31 @@
+extends CanvasLayer
+
+var buttons = {}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	buttons["Resume"] = get_node("Shade/OptionMenu/ResumeButton")
+	buttons["Move List"] = get_node("Shade/OptionMenu/MoveListButton")
+	buttons["Settings"] = get_node("Shade/OptionMenu/SettingsButton")
+	buttons["Exit Match"] = get_node("Shade/OptionMenu/ExitMatchButton")
+	buttons["Resume"].pressed.connect(_resume)
+	buttons["Move List"].pressed.connect(_show_movelist)
+	buttons["Settings"].pressed.connect(_open_settings)
+	buttons["Exit Match"].pressed.connect(_exit_match)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("pause"):
+		visible = not visible
+
+func _resume():
+	visible = false
+
+func _show_movelist():
+	pass
+
+func _open_settings():
+	pass
+
+func _exit_match():
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
---
+++ scripts/UI/main_menu.gd
@@ -0,0 +1,26 @@
+extends MarginContainer
+
+var buttons = {}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	buttons["Play"] = get_node("VSplitContainer/VBoxContainer/PlayButton")
+	buttons["Settings"] = get_node("VSplitContainer/VBoxContainer/SettingsButton")
+	buttons["Quit"] = get_node("VSplitContainer/VBoxContainer/QuitButton")
+	buttons["Play"].pressed.connect(_play)
+	buttons["Settings"].pressed.connect(_open_settings)
+	buttons["Quit"].pressed.connect(_quit_game)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _play():
+	get_tree().change_scene_to_file("res://scenes/place.tscn")
+	
+func _open_settings():
+	pass
+	
+func _quit_game():
+	get_tree().quit()
